aac74ffdd957d67b30b7fa0b4eb2174f3450cab8
booboooo <104420482+khaowhompp@users.noreply.github.com>
2026-09-23 23:51:58 +0700

    Create count_it.py

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,14 @@
+
+import sys
+
+def main():
+    params = sys.argv[1:]
+    if len(params) == 0:
+        print("none")
+    else:
+        print(f"parameters: {len(params)}")
+        for param in params:
+            print(f"{param}: {len(param)}")
+
+if __name__ == "__main__":
+    main()
