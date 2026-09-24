dc670a2e8d16e45e6b5fff41ff218ce6393c2cca
CWP-popunnur <104420482+CWP-popunnur@users.noreply.github.com>
2026-09-24 22:23:29 +0700

    Update aff_rev_params.py

--- cell05/ex08/aff_rev_params.py
+++ cell05/ex08/aff_rev_params.py
@@ -2,11 +2,11 @@
 import sys
 
 def main():
-    # ตรวจสอบว่าพารามิเตอร์มีน้อยกว่า 2 ตัวหรือไม่ (sys.argv[0] คือชื่อไฟล์สคริปต์)
+    
     if len(sys.argv) < 3:
         print("none")
     else:
-        # วนลูปย้อนหลังจากตัวสุดท้ายมาจนถึงตัวแรก (ข้าม sys.argv[0])
+        
         for param in reversed(sys.argv[1:]):
             print(param)
 
